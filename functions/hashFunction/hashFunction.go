4b5381ff3ead37b6dbdaf055f3337d23e000f394
pratiksajjanwar <pratiksajjanwar@gmail.com>
2020-08-17 14:30:29 +0530

    Update hashFunction.go

--- functions/hashFunction/hashFunction.go
+++ functions/hashFunction/hashFunction.go
@@ -61,5 +61,6 @@ func (s *hashFunction) Eval(params ...interface{}) (interface{}, error) {
 	secondHash.Write([]byte(example2))
 	fmt.Printf("%x\n", firstHash.Sum(nil))
 	fmt.Println(bytes.Equal(firstHash.Sum(nil), secondHash.Sum(nil)))
-	return firstHash.Sum(nil),nil
+	var hash := firstHash.Sum(nil)
+	return hash,nil
 }
